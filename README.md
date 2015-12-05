92b9439e93e803cc45ead7519546e6079d327d6d
Tanner Filip <tannerfilip@gmail.com>
2015-12-04 23:40:32 -0600

    Initial (probably non-working) commit

--- README.md
+++
@@ -1,2 +0,0 @@
-#TwitterFixer
-Removes "Moments" and puts "Notifications" back where it belongs.
--- index.js
+++
@@ -1,4 +0,0 @@
-var pageMod = require("sdk/page-mod").PageMod({
-  include: /^https?:\/\/.*twitter.com.*/i,
-  contentStyle: "a[href^=\"/i/moments\"] {display: none !important;}"
-});
